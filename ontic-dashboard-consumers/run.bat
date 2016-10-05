11c260a26e90076751e4c4c45f9a3e813b038dd1
dellemcontic <emc.ontic@gmail.com>
2016-10-05 11:07:08 +0200

    Revert "added projects ontic-dasboard"
    This reverts commit 60b130ef3039aa3e7db60eb44cb7e75497e6ee28.

--- ontic-dashboard-consumers/run.bat
+++
@@ -1,8 +0,0 @@
-set QUEUE_NOT_RECEIVED=queue.PgfNotificationReceived
-set QUEUE_NOT_SENT=queue.PgfNotificationSent
-set QUEUE_TRAFFIC=queue.Traffic
-set DATAFILE_PATH=C://Alberto//Clientes//Consulting Clientes//ONTIC//2016-07 ONTIC - QoS Dashboard//Deliverables//Amy final version//ontic-dashboard//target///classes//static//onticData.json
-set RECEIVED_NOT_PATH=C://Alberto//Clientes//Consulting Clientes//ONTIC//2016-07 ONTIC - QoS Dashboard//Deliverables//Amy final version//ontic-dashboard//target///classes//static//PgfReceivedData.json
-set SENT_NOT_PATH=C://Alberto//Clientes//Consulting Clientes//ONTIC//2016-07 ONTIC - QoS Dashboard//Deliverables//Amy final version//ontic-dashboard//target///classes//static//PgfSentData.json
-
-java -jar target\ontic-dashboard-consumers-1.0-SNAPSHOT.jar
